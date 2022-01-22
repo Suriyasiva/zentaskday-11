dda3d1ec742acbd5d9b55ce43b15fbcfd7939468
suriya <93177461+Suriyasiva@users.noreply.github.com>
2022-01-21 22:50:48 -0800

    Add files via upload

---
+++ day11.js
@@ -0,0 +1,50 @@
+//1. Fix the bugs in below snippet
+/*
+
+    <html lang="en">
+    <head>
+        <title>Document  guvi </title>
+        
+    </head>
+     <body>
+               
+   
+        <div> Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        
+            
+              <div>  Guvi Geek Network</div>
+            
+        </body>
+    </html>
+
+    */
+   
+//  2 .Try the below one
+/*
+<html lang="en">
+    <head>
+        <title>Document
+           
+                guvi
+    </head>
+
+     <body>
+
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+
+            <div>
+                Guvi Geek Network
+            </div>
+
+        </body>
+    </html>
+*/
+
+
+
+
+
+
+
